2f00f7b0df0966063f5efea6777d355526203ab3
amjad <amjadsam272@gmail.com>
2021-04-08 10:46:41 +0530

    Logo

--- src/components/navigation/toolbar/toolbar.js
+++ src/components/navigation/toolbar/toolbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./toolbar.css";
+import Logo from "./../../../components/Logo/Logo";
 export default function Toolbar()
 {
     return(
         <header className="Toolbar">
             <div>MENU</div>
-            <div>LOGO</div>
+            <Logo/>
             <nav>
                 ...
             </nav>
